4f5efdefd14f02357a0ab86d6e160c51a82e3e64
idir-26 <47400471+idir-26@users.noreply.github.com>
2019-04-10 14:33:40 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -93,7 +93,7 @@ class imageController extends WidgetController {
 		let domstr = _atob(result.response.dom); // decode result
 		let parser = new DOMParser(); // init dom parser
 		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="9vLh-OQ9sx9BKM:"]').firstResult; // find interesting things
+		let article = new xph().doc(dom).ctx(dom).craft('/html/body/div[5]/div[3]/div[3]/div[2]/div/div[2]/div[2]/div/div/div/div/div[1]/div[2]/div[1]/div[1]/a[1]/img').firstResult; // find interesting things
 		this.mvc.view.update(article.getAttributes("img"));
 		
 	}
